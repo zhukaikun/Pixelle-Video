f2dbb5d5cf0f98713dd3f38b01efcdc989fd3f36
IT木头人 <15543049+it-sticks-figure@user.noreply.gitee.com>
2026-07-09 18:35:40 +0800

    fix: remove GitHub link and star badge from version info section

--- web/components/content_input.py
+++ web/components/content_input.py
@@ -264,20 +264,9 @@ def render_bgm_section(key_prefix=""):
 
 
 def render_version_info():
-    """Render version info and GitHub link"""
+    """Render version info"""
     with st.container(border=True):
         st.markdown(f"**{tr('version.title')}**")
         version = get_project_version()
-        github_url = "https://github.com/AIDC-AI/Pixelle-Video"
-        
-        # Version and GitHub link in one line
-        github_url = "https://github.com/AIDC-AI/Pixelle-Video"
-        badge_url = "https://img.shields.io/github/stars/AIDC-AI/Pixelle-Video"
-
-        st.markdown(
-            f'{tr("version.current")}: `{version}` &nbsp;&nbsp; '
-            f'<a href="{github_url}" target="_blank">'
-            f'<img src="{badge_url}" alt="GitHub stars" style="vertical-align: middle;">'
-            f'</a>',
-            unsafe_allow_html=True)
+        st.markdown(f'{tr("version.current")}: `{version}`')
 
